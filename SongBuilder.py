f74f3172913a2d0a81aa078e7585a71c98dfeb26
Arthur Wood <cwarthur1029@gmail.com>
2018-11-18 10:22:58 -0500

    New scales added and cleaning performed
    Added DICT_OF_SCALES
    Added MAJOR_SCALES
    Added MINOR_SCALES
    Added MAJOR/MINOR_BLUES_SCALES
    Cleaned out unnecessary functions

--- SongBuilder.py
+++ SongBuilder.py
@@ -1,7 +1,6 @@
 
 from __future__ import print_function
 import random
-# We'll start with a couple of globals...
 
 CardTitlePrefix = "Song Builder"
 
@@ -40,6 +39,32 @@ def build_response(session_attributes, speechlet_response):
         'response': speechlet_response
     }
 
+# --------------- General Python Logic Functions ------------------------ #
+def random_dict_keys(dictionary):
+    key_list = []
+    i = 0
+    while i < len(dictionary):
+        key_list.append(dictionary[i].keys())
+        i += 1
+    random_key = random.choice(key_list)
+    key_to_give = random_key[0]
+    return key_to_give
+    
+    
+def remove_period(string):
+  string_list = list(string)
+  i = 0
+  while i < len(string_list):
+    if string_list[i] == ".":
+      string_list[i] = ""
+    i += 1
+    
+  string = "".join(string_list)
+  return string.lower()
+  
+def translate_to_seventh(string):
+    return string.replace('7th', 'seventh')
+
 # --------------- Functions that control the skill's behavior ------------------
 def get_welcome_response():
     session_attributes = {}
@@ -47,9 +72,10 @@ def get_welcome_response():
     speech_output = "Welcome to song builder... Ask me for a random chord to get started!"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "I'm sorry - I didn't understand. Please ask for a random card"
+    reprompt_text = "I'm sorry - I didn't understand. Please ask for a random chord"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
+
 
 def handle_session_end_request():
     card_title = "Session Ended"
@@ -58,26 +84,32 @@ def handle_session_end_request():
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
-
-def say_hello():
-    """
-    Return a suitable greeting...
-    """
-    card_title = "Greeting Message"
-    greeting_string = "Hello, how are you?"
-    return build_response({}, build_speechlet_response(card_title, greeting_string, "Ask me to say hello...", True))
+    
     
 def random_chord():
     card_title = "Here's your random chord"
     chord_to_give = random.choice(CHORD_DICT.keys())
     speech_output = chord_to_give
     # Setting this to true ends the session and exits the skill.
-    should_end_session = False
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+        
+def random_scale():
+    card_title = "Here's your random scale"
+    scale_class = random.choice(DICT_OF_SCALES.keys())
+    scale = random.choice(scale_class.keys())
+    scale_notes = scale_class[scale]
+    speech_output = "You should play the {0} scale. The notes are {1}".format(scale + " " + DICT_OF_KEYS[scale_class], scale_notes)
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+
 def create_chord_attributes(chord_to_explain):
     return {"explainChord": chord_to_explain}
+      
         
 def explain_chord(intent, session):
     """ Sets the chord in the session and prepares the speech to reply to the
@@ -86,10 +118,12 @@ def explain_chord(intent, session):
 
     card_title = "Here's how to play your chord."
     session_attributes = {""}
-    should_end_session = False
+    should_end_session = True
 
     if 'chord' in intent['slots']:
-        chord_to_explain = intent['slots']['chord']['value']
+        if '7th' in intent['slots']['chord']['value']:
+            intent['slots']['chord']['value'] = translate_to_seventh(intent['slots']['chord']['value'])
+        chord_to_explain = remove_period(intent['slots']['chord']['value'])
         notes_to_chord = CHORD_DICT[chord_to_explain]
         session_attributes = create_chord_attributes(chord_to_explain)
         speech_output = "The notes are " + notes_to_chord
@@ -100,8 +134,24 @@ def explain_chord(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-def blah():
-    print "Delete this"
+def explain_scale(intent):
+    
+    card_title = "Here's how to play your scale."
+    session_attributes = {""}
+    should_end_session = True
+    
+    scale_class = intent['slots']['scale_class']['value']
+    note = remove_period(intent['slots']['note']['value'])
+    if scale_class == 'major':
+        scale_dictionary = MAJOR_SCALE
+    
+    session_attributes = create_chord_attributes(note)
+    speech_output = "The notes to your scale are " + scale_dictionary[note]
+    reprompt_text = ""
+    
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
 
     
 # --------------- Events ------------------
@@ -131,6 +181,10 @@ def on_intent(intent_request, session):
         return random_chord()
     elif intent_name == "ExplainChord":
         return explain_chord(intent, session)
+    elif intent_name == "RandomScale":
+        return random_scale()
+    elif intent_name == "ListScale":
+        return explain_scale(intent)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -150,42 +204,124 @@ def on_session_ended(session_ended_request, session):
 CHORD_DICT = {
                   'a flat major': 'a flat, c, e flat', 'a major': 'a, c sharp, e', 'b flat major': 'b flat, d, f', 'b major': 'b, d sharp, f sharp', 'c major': 'c, e, g', 'c sharp major': 'c sharp, e sharp, g sharp',
                   'd major': 'd, f sharp, a', 'e flat major': 'e flat, g, b flat', 'e major': 'e, g sharp, b', 'f major': 'f, a, c', 'f sharp major': 'f sharp, a sharp, c sharp', 'g major': 'g, b, d',
-                  'a flat minor': '', 'a minor': '', 'b flat minor': '', 'b minor': '', 'c minor': '', 'c sharp minor': '',
-                  'd minor': '', 'e flat minor': '', 'e minor': '', 'f minor': '', 'f sharp minor': '', 'g minor': '',
-                  'a flat diminished': '', 'a diminished': '', 'b flat diminished': '', 'b diminished': '',
-                  'c sharp diminished': '', 'c diminished': '', 'd diminished': '', 'e flat diminished': '', 'e diminished': '',
-                  'f diminished': '', 'f sharp diminished': '', 'g diminished': '',
-                  'a flat major seventh': '', 'a major seventh': '', 'b flat major seventh': '', 'b major seventh': '',
-                  'c major seventh': '', 'c sharp major seventh': '', 'd major seventh': '', 'e flat major seventh': '', 'e major seventh': '',
-                  'f major seventh': '', 'f sharp major seventh': '', 'g major seventh': '',
-                  'a flat dominant seventh': '', 'a dominant seventh': '', 'b flat dominant seventh': '', 'b dominant seventh': '',
-                  'c dominant seventh': '', 'c sharp dominant seventh': '', 'd dominant seventh': '', 'e flat dominant seventh': '',
-                  'e dominant seventh': '', 'f dominant seventh': '', 'f sharp dominant seventh': '', 'g dominant seventh': '',
-                  'a flat minor seventh': '', 'a minor seventh': '', 'b flat minor seventh': '', 'b minor seventh': '', 'c minor seventh': '',
-                  'c sharp minor seventh': '', 'd minor seventh': '', 'e flat minor seventh': '', 'e minor seventh': '',
-                  'f minor seventh': '', 'f sharp minor seventh': '', 'g minor seventh': '',
-                  'a flat minor seventh flat five': '', 'a minor seventh flat five': '', 'b flat minor seventh flat five': '',
-                  'b minor seventh flat five': '', 'c minor seventh flat five': '', 'c sharp minor seventh flat five': '',
-                  'd minor seventh flat five': '', 'e flat minor seventh flat five': '', 'e minor seventh flat five': '',
-                  'f minor seventh flat five': '', 'f sharp minor seventh flat five': '', 'g minor seventh flat five': ''
+                  'a flat minor': 'a flat, b, e flat', 'a minor': 'a, c, e', 'b flat minor': 'b flat, d flat, f', 'b minor': 'b, d, f sharp', 'c minor': 'c, e flat, g', 'c sharp minor': 'c sharp, e, g sharp',
+                  'd minor': 'd, f, a', 'e flat minor': 'e flat, g flat, b flat', 'e minor': 'e, g, b', 'f minor': 'f, a flat, c', 'f sharp minor': 'f sharp, a, c sharp', 'g minor': 'g, b flat, d',
+                  'a flat diminished': 'a flat, c flat, d', 'a diminished': 'a, c, e flat', 'b flat diminished': 'b flat, d flat, e', 'b diminished': 'b, d, f',
+                  'c sharp diminished': 'c sharp, e, g', 'c diminished': 'c, e flat, g flat', 'd diminished': 'd, f, a flat', 'e flat diminished': 'e flat, g flat, a', 'e diminished': 'e, g, b flat',
+                  'f diminished': 'f, a flat, b', 'f sharp diminished': 'f sharp, a, c', 'g diminished': 'g, b flat, d flat',
+                  'a flat major 7th': 'a flat, c, e flat, g', 'a major 7th': 'a, c sharp, e, g sharp', 'b flat major 7th': 'b flat, d, f, a', 'b major 7th': 'b, d sharp, f sharp, a sharp',
+                  'c major 7th': 'c, e, g, b', 'c sharp major 7th': 'c sharp, f, g sharp, c', 'd major 7th': 'd, f sharp, a, c sharp', 'e flat major 7th': 'e flat, g, b flat, d', 'e major 7th': 'e, g sharp, b, d sharp',
+                  'f major 7th': 'f, a, c, e', 'f sharp major 7th': 'f sharp, a sharp, c sharp, f', 'g major 7th': 'g, b, d, f sharp',
+                  'a flat dominant seventh': 'a flat, c, e flat, g flat', 'a dominant seventh': 'a, c sharp, e, g', 'b flat dominant seventh': 'b flat, d, f, a flat', 'b dominant seventh': 'b, d sharp, f sharp, a',
+                  'c dominant seventh': 'c, e, g, b flat', 'c sharp dominant seventh': 'c sharp, e sharp, g sharp, b', 'd dominant seventh': 'd, f sharp, a, c', 'e flat dominant seventh': 'e flat, g, b flat, d flat',
+                  'e dominant seventh': 'e, g sharp, b, d', 'f dominant seventh': 'f, a, c, e flat', 'f sharp dominant seventh': 'f sharp, a sharp, c sharp, e', 'g dominant seventh': 'g, b, d, f',
+                  'a flat minor seventh': 'a flat, c flat, e flat, g flat', 'a minor seventh': 'a, c, e, g', 'b flat minor seventh': 'b flat, d flat, f, a flat', 'b minor seventh': 'b, d, f sharp, a', 'c minor seventh': 'c, e flat, g, b flat',
+                  'c sharp minor seventh': 'c sharp, e, g sharp, b', 'd minor seventh': 'd, f, a, c', 'e flat minor seventh': 'e flat, g flat, b flat, d flat', 'e minor seventh': 'e, g, b, d',
+                  'f minor seventh': 'f, a flat, c, e flat', 'f sharp minor seventh': 'f sharp, a, c sharp, e', 'g minor seventh': 'g, b flat, d, f',
+                  'a flat minor seventh flat five': 'a flat, c flat, e double flat, g flat', 'a minor seventh flat five': 'a, c, e flat, g', 'b flat minor seventh flat five': 'b flat, d flat, f flat, a flat',
+                  'b minor seventh flat five': 'b, d, f, a', 'c minor seventh flat five': 'c, e flat, g flat, b flat', 'c sharp minor seventh flat five': 'c sharp, e, g, b',
+                  'd minor seventh flat five': 'd, f, a flat, c', 'e flat minor seventh flat five': 'e flat, g flat, b double flat, d flat', 'e minor seventh flat five': 'e, g, b flat, d',
+                  'f minor seventh flat five': 'f, a flat, c flat, e flat', 'f sharp minor seventh flat five': 'f sharp, a, c, e', 'g minor seventh flat five': 'g, b flat, d flat, f'
                   }
-                  
-                  
-CHORD_NOTE_DICTIONARY = {"a": "a, c sharp, and e", "b": "b, d sharp, and f sharp", \
-                         "c": "c, e, and g"}
                          
 # --------------- Lists containing standard scale dictionaries -------------------- #
 # - https://www.pianoscales.org/major.html - #
+sharp_notes = ['c', 'c sharp', 'd', 'd sharp', 'e', 'f', 'f sharp', 'g', 'g sharp', 'a', 'a sharp', 'b']
+flat_notes = ['c', 'd flat', 'd', 'e flat', 'e', 'f', 'g flat', 'g', 'a flat', 'a', 'b flat', 'b']
 
-MAJOR_SCALE = []
+"""
+                    {'c': '',
+                     'c sharp': '',
+                     'd flat': '',
+                     'd': '',
+                     'd sharp': '',
+                     'e flat': '',
+                     'e': '',
+                     'f': '',
+                     'f sharp': '',
+                     'g': '',
+                     'a flat': '',
+                     'a': '',
+                     'b flat': '',
+                     'b': ''
+                    }"""
+                    
+# This was turned into a dictionary to store the "speech" version of the scale class
+DICT_OF_SCALES = {MAJOR_SCALE: "Major", MINOR_SCALE: "Minor", MINOR_BLUES_SCALE: "Minor Blues"}
 
-MINOR_SCALE = []
+MAJOR_SCALE = {'c': 'C, D, E, F, G, A, B, C',
+               'c sharp': 'C sharp, D sharp, F, F sharp, G sharp, A sharp, B sharp, C sharp',
+               'd flat': 'D flat, E flat, F, G flat, A flat, B flat, C, D flat',
+               'd': 'D, E, F sharp, G, A, B, C sharp',
+               'd sharp': 'D sharp, E sharp, F double sharp, G sharp, A sharp, B sharp, C double sharp',
+               'e flat': 'E flat, F, G, A flat, B flat, C, D',
+               'e': 'E, F sharp, G sharp, A, B, C sharp, D sharp',
+               'f': 'F, G, A, B flat, C, D, E',
+               'f sharp': 'F sharp, G sharp, A sharp, B, C sharp, D sharp, F',
+               'g flat': 'Gb, Ab, Bb, Cb, Db, Eb, F, Gb',
+               'g': 'G, A, B, C, D, E, F sharp',
+               'g sharp': 'G sharp, A sharp, B sharp, C sharp, D sharp, E sharp, F double sharp, G sharp',
+               'a flat': 'A flat, B flat, C, D flat, E flat, F, G',
+               'a': 'A, B, C sharp, D, E, F sharp, G sharp',
+               'a sharp': 'A sharp, B sharp, C double sharp, D sharp, E sharp, F double sharp, G doubel sharp, A sharp',
+               'b flat': 'B flat, C, D, E flat, F, G, A',
+               'b': 'B, C sharp, D sharp, E, F sharp, G sharp, A sharp'
+                }
 
-BLUES_SCALE = []
+MINOR_SCALE = {'c': 'C, D, E flat, F, G, A flat, B flat, C',
+               'c sharp': 'C sharp, D sharp, E, F sharp, G sharp, A, B, C sharp',
+               'd flat': 'D flat, E flat, F flat, G flat, A flat, A, B, D flat',
+               'd': 'D, E, F, G, A, B flat, C, D',
+               'd sharp': 'D sharp, E sharp, F sharp, G sharp, A sharp, B, C sharp, D sharp',
+               'e flat': 'E flat, F, G flat, A flat, B flat, C flat, D flat, E flat',
+               'e': 'E, F sharp, G, A, B, C, D, E',
+               'f': 'F, G, A flat, B flat, C, D flat, E flat, F',
+               'f sharp': 'F sharp, G sharp, A, B, C sharp, D, E, F sharp',
+               'g flat': 'G flat, A flat, B double flat, C flat, D flat, E double flat, F flat, G flat',
+               'g': 'G, A, B flat, C, D, Eb, F, G',
+               'g sharp': 'G sharp, A sharp, B, C sharp, D sharp, E, F sharp, G sharp',
+               'a flat': 'A flat, B flat, C flat, D flat, E flat, F flat, G flat, A flat',
+               'a': 'A, B, C, D, E, F, G, A',
+               'a sharp': 'A sharp, B sharp, C sharp, D sharp, F, F sharp, G sharp, A sharp',
+               'b flat': 'B flat, C, D flat, E flat, F, G flat, A flat, B flat',
+               'b': 'B, C sharp, D, E, F sharp, G, A, B'
+                }
 
-JAZZ_SCALE = []
+MINOR_BLUES_SCALE = {'c': '',
+                     'c sharp': '',
+                     'd flat': '',
+                     'd': '',
+                     'd sharp': '',
+                     'e flat': '',
+                     'e': '',
+                     'f': '',
+                     'f sharp': '',
+                     'g': '',
+                     'a flat': '',
+                     'a': '',
+                     'b flat': '',
+                     'b': ''
+                    }
 
-PENTATONIC_SCALE = []
+MAJOR_BLUES_SCALE = {'c': '',
+                     'c sharp': '',
+                     'd flat': '',
+                     'd': '',
+                     'd sharp': '',
+                     'e flat': '',
+                     'e': '',
+                     'f': '',
+                     'f sharp': '',
+                     'g': '',
+                     'a flat': '',
+                     'a': '',
+                     'b flat': '',
+                     'b': ''
+                    }
+
+JAZZ_SCALE = {}
+
+PENTATONIC_SCALE = {}
 
 # --------------- Lists containing exotic scale dictionaries -------------------- #
 
